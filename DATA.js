93b9435805d8d85ce25d2050dca2b19a2cbbbe5b
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-06 16:32:40 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,15 @@
+export const item = {
+    "id": "0c6d39ef-a491-44f8-b5b2-8047d0413f45",
+    "gyroscope_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png",
+    "gyroscope_title": "List method answer Congress everything human us. Provide word history.",
+    "about_goal": "Have film might stuff nor. Capital information single mention least north behavior. Want ago save like read then owner.",
+    "dumble_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png",
+    "about_dumble": "Force miss medical. Company west significant hit. Himself interest while. Skill door let follow door executive.",
+    "fat_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png",
+    "about_fat": "How pass art a.",
+    "dna_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png",
+    "about_dna": "Kitchen put grow my move. Rule development fire fine likely factor again. Data year human population leader her.",
+    "mind_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png",
+    "about_mind": "Spring building arrive century. Voice popular fine Congress in maybe music prove. Left enjoy star lose whatever form home test.",
+    "about_member": "Court return drive very already always produce. Everybody degree hand another. Behind of perform career hope."
+}
